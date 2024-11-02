e6bf6bca511cd9d620720b9bab8a5f3ccc1caa86
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2024-11-02 13:56:17 -0300

    Update __init__.py do módulo actions
    Código pra facilitar a importação das classes.

--- quantumnet/objects/action/__init__.py
+++ quantumnet/objects/action/__init__.py
@@ -1,4 +1,19 @@
-from .swap_action import SwapAction
-from .purification_action import PurificationAction
-from .create_epr_action import CreateEPRAction
-from .drop_request_action import DropRequestAction
+from importlib import import_module
+from pkgutil import iter_modules
+from sys import modules
+from os import path
+from .action import Action
+
+# Define o diretório deste módulo
+module_dir = path.dirname(__file__)
+
+# Itera sobre todos os módulos no diretório
+for _, module_name, _ in iter_modules([module_dir]):
+    # Importa o módulo
+    module = import_module(f".{module_name}", package=__name__)
+    
+    # Obtém todas as classes no módulo e as adiciona ao namespace do pacote
+    for attr_name in dir(module):
+        attr = getattr(module, attr_name)
+        if isinstance(attr, type) and issubclass(attr, Action) and attr is not Action:
+            setattr(modules[__name__], attr_name, attr)
