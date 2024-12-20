d0fcf1f807b8c93e05809f0319724a5f9e4c5ab5
Arthur Henrique <artuenrick@gmail.com>
2024-12-20 18:29:07 -0300

    Teste de Time-Slot

--- quantumnet/control/rule/rule.py
+++ quantumnet/control/rule/rule.py
@@ -1,28 +1,29 @@
 from abc import ABC, abstractmethod
+ 
 class Rule(ABC):
-    def __init__(self, rule_name):
+    def __init__(self, rule_name, ttl = 100):
         self.rule_name = rule_name
         self.actions = {}
         self.route = []
-        self.ttl = None
+        self.ttl = ttl
         self.hit_count = 0
         
     def __repr__(self):
         return f"{self.rule_name}"
     
+    def decrement_ttl(self):
+        """Decrementa o TTL da regra."""
+        self.ttl -= 1
+    
     def run(self):
         """Executa as ações no tempo certo."""
+        # Regra executada
+        self.hit_count += 1
         for time in sorted(self.actions.keys()):
             print(f"[{self.__repr__()}] Passo: {time}")
             for action in self.actions[time]:
                 print(f"[{self.__repr__()}] Ação: {action}")
                 action.run()
-                
-    def decrement_ttl(self):
-        """
-        Decrementa o TTL da regra.
-        """
-        self.ttl -= 1
     
     def set_route(self, route):
         """
