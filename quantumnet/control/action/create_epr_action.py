a3d903aed25b5aa0b3496f7ab89d652c95478496
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2025-01-02 01:43:27 -0300

    Update create_epr_action.py

--- quantumnet/control/action/create_epr_action.py
+++ quantumnet/control/action/create_epr_action.py
@@ -20,4 +20,4 @@ class CreateEPRAction(Action):
         """
         Cria um par EPR entre dois nós.
         """
-        self.controller.network.physical.echp_on_demand(self.alice, self.bob)
+        self.controller.network.physicallayer.echp_on_demand(self.alice, self.bob)
